2505013b9c02520828ebe21829c2580de70fde4b
morozkin.vladislav@yandex.ru <mazafaka123%>
2022-05-07 09:40:41 +0300

    Тестовое задание

--- src/main/java/controllers/ClanController.java
+++ src/main/java/controllers/ClanController.java
@@ -24,19 +24,29 @@ public class ClanController {
 
     /**
      * Метод меняет кол-во золото у клана.
-     * Я сделал возвращение String, предполагая что игрок должен понимать, что произошло... :)
      *
      * @param clanId - ID клана
      * @param gold   - значение, на которое меняется кол-во золота в клане
      * @param value  1 - прирост золота, 2 - убыль золота
      */
-    public synchronized String changeGold(Long clanId, Integer gold, Long userId, int value) {
-        boolean result = value == 1 ? clanManager.plusGoldById(clanId, gold) : clanManager.minusGoldById(clanId, gold);
+    public synchronized void changeGold(Long clanId, Integer gold, Long userId, int value) {
+
+        boolean result = false;
+        String msg = "";
+
+        if (value == 1) {
+            result = clanManager.plusGoldById(clanId, gold);
+            msg = "Казна клана успешно Вами пополнена, милорд!";
+        } else if (value == 2) {
+            result = clanManager.minusGoldById(clanId, gold);
+            msg = "Вы забрали золото из клана!";
+        }
+
         if (result == true) {
             trackerManager.trackerClanGold(clanId, userId, gold, value);
-            return "Казна клана успешно Вами пополнена, милорд!";
+            System.out.println(msg);
         } else {
-            return "К сожалению Вы исключены из клана/Вам запрещено пополнять казну клана!";
+            System.out.println("К сожалению Вы исключены из клана/Вам запрещено пополнять казну клана!");
         }
     }
 
